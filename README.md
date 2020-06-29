6d196c26d9d694f99e699c92cec19c3c792e7c67
Dan Behlings <nevenall@hotmail.com>
2020-06-29 12:06:42 -0700

    initial module

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# foundryvtt-escape-window
+# Escape Window
+
+A simple module for FoundryVTT to change the behavior of the escape key from closing open windows to minimizing them.
+
+## Options?
+
+The future might include some options, but for now, just this. 
---
+++ index.js
@@ -0,0 +1,34 @@
+Hooks.on("ready", function () {
+   // override ESC key handler to not shut windows
+   keyboard._onEscape = function (event, up, modifiers) {
+      if (up || modifiers.hasFocus) return;
+
+      // Save fog of war if there are pending changes
+      if (canvas.ready) canvas.sight.saveFog();
+
+      // Case 1 - dismiss an open context menu
+      if (ui.context && ui.context.menu.length) ui.context.close();
+
+      // Case 2 - minimize open UI windows
+      else if (Object.values(ui.windows).some(w => !w._minimized)) {
+         Object.values(ui.windows).forEach(app => {
+            app.minimize();
+         });
+      }
+
+      // Case 3 (GM) - release controlled objects
+      else if (canvas.ready && game.user.isGM && Object.keys(canvas.activeLayer._controlled).length) {
+         event.preventDefault();
+         canvas.activeLayer.releaseAll();
+      }
+
+      // Case 4 - toggle the main menu
+      else ui.menu.toggle();
+
+      // Flag the keydown workflow as handled
+      this._handled.add(modifiers.key);
+   }
+
+
+
+})
