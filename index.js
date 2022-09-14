e22b5dcae47cc4b196f04a220e550327496d3228
nevenall <nevenall@hotmail.com>
2022-09-14 12:27:03 -0700

    module.json, version 10 compatibility

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ Hooks.on('init', function () {
 })
 
 function _onDismiss(context) {
+
    // Save fog of war if there are pending changes
-   if (canvas.ready) canvas.sight.saveFog();
+   if (canvas.ready) canvas.fog.commit();
 
    // Case 1 - dismiss an open context menu
    if (ui.context && ui.context.menu.length) {
@@ -13,7 +14,13 @@ function _onDismiss(context) {
       return true;
    }
 
-   // Case 2 - minimize open UI windows
+   // Case 2 - dismiss an open Tour
+   if (Tour.tourInProgress) {
+      Tour.activeTour.exit();
+      return true;
+   }
+
+   // Case 3 - minimize open UI windows
    if (Object.values(ui.windows).some(w => !w._minimized)) {
       Object.values(ui.windows).forEach(app => {
          if (app.options.minimizable)
@@ -22,13 +29,15 @@ function _onDismiss(context) {
       return true;
    }
 
-   // Case 3 (GM) - release controlled objects (if not in a preview)
-   if (game.user.isGM && canvas.activeLayer && Object.keys(canvas.activeLayer._controlled).length) {
+   // Case 4 (GM) - release controlled objects (if not in a preview)
+   if (game.user.isGM && canvas.activeLayer && canvas.activeLayer.controlled.length) {
       if (!canvas.activeLayer.preview?.children.length) canvas.activeLayer.releaseAll();
       return true;
    }
 
-   // Case 4 - toggle the main menu
+   // Case 5 - toggle the main menu
    ui.menu.toggle();
+   // Save the fog immediately rather than waiting for the 3s debounced save as part of commitFog.
+   if (canvas.ready) canvas.fog.save();
    return true;
 }
